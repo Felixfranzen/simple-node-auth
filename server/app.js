42a2d824d5e01fe431ad9bc1fc4c911d05043333
Felixfranzen <felixfranzn@gmail.com>
2018-07-20 00:00:52 +0200

    changes

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(utils.accessControlAllowOrigin);
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
+// setup api routes
 const router = express.Router();
 app.use('/api', router)
 appRouteSetup(router);
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ const authRoutes = require('./auth/index');
 const secretRoutes = require('./secret/index');
 
 module.exports = (router) => {
-  //public
   authRoutes(router);
   secretRoutes(router);
 }
